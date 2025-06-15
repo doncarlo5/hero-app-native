7507609e7770fee6b7a108fb004da0634ee23454
doncarlo5 <pro.julien.thomas@gmail.com>
2025-06-15 16:05:56 +0200

    remove list from settings

--- app/(protected)/(tabs)/settings.tsx
+++ app/(protected)/(tabs)/settings.tsx
@@ -1,40 +1,19 @@
-import { FlatList, View, TouchableOpacity } from "react-native";
-import { useEffect, useState } from "react";
-import { useRouter } from "expo-router";
+import { View } from "react-native";
 
 import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
 import { H1, Muted } from "@/components/ui/typography";
 import { useAuth } from "@/context/supabase-provider";
-import { fetchApi } from "@/lib/api-handler";
 
 export default function Settings() {
 	const { signOut } = useAuth();
-	const router = useRouter();
-
-	const [isLoadingSessions, setIsLoadingSessions] = useState(false);
-	const [sessions, setSessions] = useState<any[]>([]);
-
-	const fetchUserSessions = async () => {
-		try {
-			setIsLoadingSessions(true);
-			const response = await fetchApi(`/api/sessions`);
-			setSessions(response);
-		} catch (error: any) {
-			console.error("Fetch error: ", error);
-		} finally {
-			setIsLoadingSessions(false);
-		}
-	};
-
-	useEffect(() => {
-		fetchUserSessions();
-	}, []);
 
 	return (
-		<View className="flex-1 items-center justify-center bg-background p-4 gap-y-4">
-			<H1 className="text-center">Sign Out</H1>
-			<Muted className="text-center">
+		<View className="flex-1 items-center justify-center bg-background dark:bg-background-dark p-4 gap-y-4">
+			<H1 className="text-center text-foreground dark:text-foreground-dark">
+				Sign Out
+			</H1>
+			<Muted className="text-center text-muted-foreground dark:text-muted-foreground-dark">
 				Sign out and return to the welcome screen.
 			</Muted>
 			<Button
@@ -47,38 +26,6 @@ export default function Settings() {
 			>
 				<Text>Sign Out</Text>
 			</Button>
-			<FlatList
-				data={sessions}
-				keyExtractor={(item) => item._id}
-				renderItem={({ item }) => (
-					<TouchableOpacity className="flex-row items-center p-3 mb-2 rounded bg-muted">
-						<View
-							className={`h-3 w-3 rounded-full mr-3 ${item.is_done ? "bg-green-500" : "bg-orange-500"}`}
-						/>
-						<Text className="mr-3">{item.comment ? "��" : "🗨️"}</Text>
-						<Text className="mr-3">
-							{new Date(item.date_session).toLocaleDateString("fr-FR")}
-						</Text>
-						<Text
-							className={`mr-3 px-2 py-1 rounded ${
-								item.type_session === "Upper A"
-									? "bg-blue-200"
-									: item.type_session === "Upper B"
-										? "bg-blue-400"
-										: item.type_session === "Lower"
-											? "bg-green-200"
-											: "bg-gray-200"
-							}`}
-						>
-							{item.type_session}
-						</Text>
-						<Text className="text-center mr-3">
-							{item.exercise_user_list.length}
-						</Text>
-						<Text>➡️</Text>
-					</TouchableOpacity>
-				)}
-			/>
 		</View>
 	);
 }
