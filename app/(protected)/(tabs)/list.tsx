6e4a8ccb28cd5a9a6f4a47dfbc4ff1dfcad8fcd0
doncarlo5 <pro.julien.thomas@gmail.com>
2025-06-21 00:15:09 +0200

    styling list

--- app/(protected)/(tabs)/list.tsx
+++ app/(protected)/(tabs)/list.tsx
@@ -5,13 +5,15 @@ import {
 	ActivityIndicator,
 } from "react-native";
 import { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
 
 import { Text } from "@/components/ui/text";
-import { H1, Muted } from "@/components/ui/typography";
 import { fetchApi } from "@/lib/api-handler";
 import { colors } from "@/constants/colors";
+import { MessageSquareText } from "@/lib/icons/messageSquareText";
 
 export default function List() {
+	const router = useRouter();
 	const [isLoadingSessions, setIsLoadingSessions] = useState(false);
 	const [sessions, setSessions] = useState<any[]>([]);
 	const [sortConfig, setSortConfig] = useState({
@@ -37,6 +39,10 @@ export default function List() {
 		fetchUserSessions();
 	}, []);
 
+	const handleSessionPress = (sessionId: string) => {
+		router.push(`/session/${sessionId}`);
+	};
+
 	console.log("sessions", sessions);
 
 	return (
@@ -49,34 +55,36 @@ export default function List() {
 					data={sessions}
 					keyExtractor={(item) => item._id}
 					renderItem={({ item }) => (
-						<TouchableOpacity className="flex-row items-center p-2 mb-2 rounded bg-muted dark:bg-muted-dark w-full">
+						<TouchableOpacity
+							className="flex-row items-center px-2 py-2 border-b border-border dark:border-border-dark w-full"
+							onPress={() => handleSessionPress(item._id)}
+						>
 							<View
 								className={`h-3 w-3 rounded-full mr-3 ${item.is_done ? "bg-green-500" : "bg-orange-500"}`}
 							/>
 							<Text className="mr-3 text-foreground dark:text-foreground-dark">
-								{item.comment ? "" : "🗨️"}
+								<MessageSquareText
+									className={`${!item.comment && "opacity-30"}`}
+								/>
 							</Text>
 							<Text className="mr-3 text-foreground dark:text-foreground-dark">
 								{new Date(item.date_session).toLocaleDateString("fr-FR")}
 							</Text>
 							<Text
-								className={`mr-3 px-2 py-1 rounded ${
+								className={`mr-3 px-2 py-1 rounded-md font-medium text-xs ${
 									item.type_session === "Upper A"
-										? "bg-blue-200 dark:bg-blue-800"
+										? "bg-blue-200/30 text-blue-600 dark:bg-blue-800/20 dark:text-blue-200"
 										: item.type_session === "Upper B"
-											? "bg-blue-400 dark:bg-blue-600"
+											? "bg-violet-400/20 text-violet-500 dark:bg-violet-600/50 dark:text-violet-200"
 											: item.type_session === "Lower"
-												? "bg-green-200 dark:bg-green-800"
-												: "bg-gray-200 dark:bg-gray-700"
-								} text-foreground dark:text-foreground-dark`}
+												? "bg-green-600/10 text-green-700 dark:bg-green-800/50 dark:text-green-200"
+												: "bg-gray-200/50 text-gray-800 dark:bg-gray-700/50 dark:text-gray-200"
+								}`}
 							>
 								{item.type_session}
 							</Text>
 							<Text className="text-center mr-3 text-foreground dark:text-foreground-dark">
 								{item.exercise_user_list.length}
-							</Text>
-							<Text className="text-foreground dark:text-foreground-dark">
-								➡️
 							</Text>
 						</TouchableOpacity>
 					)}
